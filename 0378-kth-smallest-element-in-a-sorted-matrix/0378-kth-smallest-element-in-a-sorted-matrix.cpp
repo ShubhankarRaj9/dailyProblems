6106075fc4f94da4fc950ee4ff5324db8040277b
Shubhankar <sparshrajsr@gmail.com>
2026-07-01 09:33:19 +0530

    Time: 0 ms (100%), Space: 17.1 MB (66.38%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
-    int findElements(vector<vector<int>>& matrix, int val){
-    int n = matrix.size();
-    int cnt = 0;
-    int row = n-1, col = 0; 
-    
-    while(row >= 0 && col < n){
-         if(matrix[row][col] <= val){
-            cnt += (row + 1);
-            col++; 
+    int findElements(vector<vector<int>>& matrix, int val) {
+        int n = matrix.size();
+        int cnt = 0;
+        int row = n - 1, col = 0;
+
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= val) {
+                cnt += (row + 1);
+                col++;
+            } else {
+                row--;
+            }
         }
-        else{
-            row--;
-        }
+        return cnt;
     }
-    return cnt;
-}
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
         int m = matrix[0].size();
 
         int low = matrix[0][0];
-        int high = matrix[n-1][m-1];
+        int high = matrix[n - 1][m - 1];
 
         int res = -1;
 
-        while(low <= high){
-            int guess = low + (high-low)/2;
-            int val = findElements(matrix,guess);
-            if(val < k){
-                low = guess+1;
-            }
-            else{
+        while (low <= high) {
+            int guess = low + (high - low) / 2;
+            int val = findElements(matrix, guess);
+
+            if (val < k) {
+                low = guess + 1;
+            } else {
                 res = guess;
-                high = guess-1;
+                high = guess - 1;
             }
         }
         return res;
